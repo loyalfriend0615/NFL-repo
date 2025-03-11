de907ea13e7da63194818e462330fc392c4a7c49
loyalfriend0615 <loyalfriend0615@gmail.com>
2025-03-11 11:17:27 -0400

    changed date-fns version

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,29 @@
+'use client'
 import { PlayerDashboard } from "@/components/player-dashboard"
+import { useEffect, useState } from "react"
 
 export default function Home() {
-  return (
-    <main className="w-full min-h-screen flex flex-col">
-      <div className="p-4 md:p-6 bg-background border-b">
-        <h1 className="text-3xl font-bold tracking-tight">NFL Player Performance Analysis</h1>
-        <p className="text-muted-foreground">Advanced visualization tools for NFL player performance metrics</p>
-      </div>
+  const [isRendered, setIsRendered] = useState(false);
 
-      <div className="flex-1">
-        <PlayerDashboard />
-      </div>
-    </main>
+  useEffect(() => {
+    setIsRendered(true);
+  }, [])
+
+  return (
+    isRendered ? (
+      <main className="w-full min-h-screen flex flex-col">
+        <div className="p-4 md:p-6 bg-background border-b">
+          <h1 className="text-3xl font-bold tracking-tight">NFL Player Performance Analysis</h1>
+          <p className="text-muted-foreground">Advanced visualization tools for NFL player performance metrics</p>
+        </div>
+
+        <div className="flex-1">
+          <PlayerDashboard />
+        </div>
+      </main>
+    ) : (
+      <div>Loading...</div>
+    )
   )
 }
 
